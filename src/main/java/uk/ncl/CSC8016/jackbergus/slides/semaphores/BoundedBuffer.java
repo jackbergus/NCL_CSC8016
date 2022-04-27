4a30edefaf2487bf8b7b67dc739c042e5c20d00f
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-04-27 21:38:46 +0100

    Bounded buffer

---
+++ src/main/java/uk/ncl/CSC8016/jackbergus/slides/semaphores/BoundedBuffer.java
@@ -0,0 +1,69 @@
+package uk.ncl.CSC8016.jackbergus.slides.semaphores;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+
+public class BoundedBuffer<T> {
+    Queue<T> message = new LinkedList<>();
+    Semaphore empty, full, mutex;
+    int size;
+
+    BoundedBuffer(int capacity) {
+        size = capacity;
+        mutex = new Semaphore(1);
+        empty = new Semaphore(size);
+        full = new Semaphore(0);
+    }
+
+    public void put(T m) {
+        try {
+            empty.acquire();
+            mutex.acquire();
+            message.add(m);
+            mutex.release();
+            full.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public T get() {
+        T val = null;
+        try {
+            full.acquire();
+            mutex.acquire();
+            val = message.remove();
+            mutex.release();
+            empty.release();
+        }catch (InterruptedException e) {
+
+        }
+        return val;
+    }
+
+    public static int produce(int max, int min) {
+        return (int)Math.floor(Math.random()*(max-min+1)+min);
+    }
+
+    public static void main(String args[]) {
+        var buffer = new BoundedBuffer<Integer>(10);
+        var producer = new Thread(() -> {
+            while (true) {
+                var val = produce(100, 0);
+                System.out.println("Putting: "+val);
+                buffer.put(val);
+            }
+        });
+        var consumer = new Thread(() -> {
+            while (true) {
+                var val = buffer.get();
+                System.out.println(val);
+            }
+        });
+        producer.start();
+        consumer.start();
+        // And they lived happily ever after...
+    }
+
+}
