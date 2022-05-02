36fde1f2219fc8e5e85c2b9436def86f454d5cbb
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-05-02 19:18:00 +0100

    New example for synchronized vs semaphores

---
+++ src/main/java/uk/ncl/CSC8016/jackbergus/slides/locks/ReentrantLockCounter.java
@@ -0,0 +1,64 @@
+package uk.ncl.CSC8016.jackbergus.slides.locks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockCounter {
+    private int c;
+    Lock lock;
+
+    public ReentrantLockCounter() {
+        c = 0;
+        lock = new ReentrantLock(true);
+    }
+    public void increment() {
+        lock.lock();
+        try {
+            c++;
+        } finally {
+            lock.unlock();
+        }
+    }
+    public synchronized void decrement() {
+        lock.lock();
+        try {
+            c--;
+        } finally {
+            lock.unlock();
+        }
+    }
+    public synchronized int value() {
+        int copy;
+        lock.lock();
+        try {
+            copy = c;
+        } finally {
+            lock.unlock();
+        }
+        return copy;
+    }
+    public synchronized void increment(int n) {
+        lock.lock();
+        try {
+            if (n>0) {
+                c++;
+                increment(n-1);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        ReentrantLockCounter rlc = new ReentrantLockCounter();
+        var t1 = new Thread(() -> {
+            rlc.increment(200);
+            System.out.println(rlc.value()+"! [1]");
+        });
+        var t2 = new Thread(() -> {
+            System.out.println("I got "+rlc.value());
+        });
+        t2.start(); t1.start();
+        t1.join(); t2.join();
+    }
+}
