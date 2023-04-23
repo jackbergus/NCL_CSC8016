7dfd0c937b1a335f1812033aa949a2a28076cc3c
Giacomo Bergami <bergamigiacomo@gmail.com>
2023-04-23 19:32:55 +0100

    New Example

---
+++ src/main/java/uk/ncl/CSC8016/jackbergus/slides/introduction/Slide17.java
@@ -0,0 +1,49 @@
+package uk.ncl.CSC8016.jackbergus.slides.introduction;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Slide17 {
+        public static void main(String[] args) throws Exception {
+            boolean lastExample = false;
+            boolean doWait = true;
+            for (int i = 0; i<1000; i++ ) {
+                // Execrise:
+                // 1) What happens if I replace this with a read lock from the same ReadWriteLock?
+                // 2) What happens if I replace this with a write lock  from the same ReadWriteLock?
+                var rl = new ReentrantLock(true);
+                System.out.println("Run #"+i);
+                var t1 = new Thread(() -> {
+                    if (lastExample) rl.lock();
+                    System.out.println("A");
+                    if (doWait) try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("B");
+                if (lastExample) try{
+                    rl.unlock();
+                    } finally {
+                }
+                });
+                var t2 = new Thread(() -> {
+                    if (lastExample) rl.lock();
+                    System.out.println(1);
+                    if (doWait) try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(2);
+
+                    if (lastExample) try{
+                        rl.unlock();
+                    } finally {
+
+                    }
+                });
+                t1.start(); t2.start();
+                t1.join(); t2.join();
+            }
+        }
+}
